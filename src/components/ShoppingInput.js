639895c9432a12a3fec22c50b7a00e0bbff98d54
Felipe Soller <soller@outlook.com>
2020-10-30 11:32:26 -0300

    Creation of the initial components

---
+++ src/components/ShoppingInput.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class ShoppingInput extends Component {
+    render() {
+        return (
+            <div>
+                Hello From Shopping Input
+            </div>
+        )
+    }
+}
---
+++ src/components/ShoppingList.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class ShoppingList extends Component {
+    render() {
+        return (
+            <div>
+                Hello from ShoppingList
+            </div>
+        )
+    }
+}
